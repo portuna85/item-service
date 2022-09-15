fde193642fc15a1016411bd83cfd66fa79b7cff3
portuna85 <portuna85@gmail.com>
2022-09-15 16:55:45 +0900

    item-service update

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -69,7 +69,7 @@ public class BasicItemController {
     @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
-        return "basic/item";
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
